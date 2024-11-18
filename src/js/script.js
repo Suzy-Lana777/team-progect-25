c57c3409f4ca2291a417e092e5e9e9d939f093ed
Suzy-Lana777 <reutasvetlana382@gmail.com>
2024-11-18 18:16:13 +0200

    new js cod

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,14 @@
 (() => {
-  константные ссылки = {
-    // Добавить атрибуты data-modal-open на кнопку открытия
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    //Добавить атрибуты data-modal-close на кнопку закрытия
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    // Добавляем атрибуты data-modal на бекдроп модалки
-    модальный: document.querySelector("[data-menu]"),
+  const refs = {
+    openModalBtn: document.querySelector("[open-modal]"),
+    
+    closeModalBtn: document.querySelector("[close]"),
+    modal: document.querySelector("[modal]"),
   };
-
-  refs.openModalBtn.addEventListener("щелчок", toggleModal);
-  refs.closeModalBtn.addEventListener("щелчок", toggleModal);
-
-  функция toggleModal() {
-    // открыт этот класс, который нужно будет додать/запустить на бэкдропе при нажатии на кнопку
+  refs.openModalBtn.addEventListener("click", toggleModal);
+ 
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  function toggleModal() {
     refs.modal.classList.toggle("is-open");
   }
-})();
+})(); 
